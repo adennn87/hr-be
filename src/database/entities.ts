6b20d12539311e416ca28f1eb1ecca6024af934c
AdenDz87 <hminhvnc@gmail.com>
2026-03-04 16:54:22 +0700

    entity fix

--- src/database/entities.ts
+++ src/database/entities.ts
@@ -5,5 +5,7 @@ import { RoleFunction } from 'src/roles/entities/role_function.entity';
 import { Function } from 'src/function/entities/function.entity';
 import { IamPolicyRuleEntity } from 'src/dashboard/entities/iam-policy-rule.entity';
 import { DashboardModuleEntity } from 'src/dashboard/entities/dashboard-module.entity';
+import { AllocatedAsset } from 'src/allocated-assets/entities/allocated-asset.entity';
+import { Asset } from 'src/allocated-assets/entities/asset.entity';
 
-export const entities = [User, Otp, Function, Role, RoleFunction, DashboardModuleEntity, IamPolicyRuleEntity]; 
+export const entities = [User, Otp, Function, Role, RoleFunction, IamPolicyRuleEntity, DashboardModuleEntity, Asset, AllocatedAsset]; 
